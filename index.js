d7527a81e40df1692dfedbc0d4023a145fcc8990
Alladin Melico <melico.alladin@gmail.com>
2021-01-27 03:15:33 +0800

    updated

--- index.js
+++ index.js
@@ -46,14 +46,14 @@ function getNews(country) {
                         <div class="card mx-auto" style="width: 18rem;">
                             <img src="${element.urlToImage}" class="card-img-top" alt="...">
                             <div class="card-body">
-                                <h5 class="card-title">${element.title}</h5>
+                                <h5 class="card-title"><a href="${element.url}" target="_blank">${element.title}</a></h5>
                                 <p class="card-text">
                                     ${element.description}
                                 </p>
                             </div>
                         </div>
                     </div>
-                `)
+				`)
 			})
 		},
 	})
@@ -71,8 +71,10 @@ function fda() {
 			let result = response.results
 			result.forEach((element) => {
 				let product = []
+				let products = ''
 				element.patient.drug.forEach((element) => {
-					product.push(element.medicinalproduct)
+					// product.push(element.medicinalproduct)
+					products += `<li>${element.medicinalproduct}</li>`
 				})
 				$('#main').append(`
 			        <div class="col-12 col-md-6 col-lg-4">
@@ -81,24 +83,18 @@ function fda() {
 			                    <h5 class="card-title">${element.companynumb}</h5>
                                 <p class="card-text">
                                     <ul>
-                                        <li>${
-											element.patient.reaction[0]
-												.reactionmeddrapt
-										}</li>
-                                        <li>${
-											element.patient.reaction[0]
-												.reactionmeddraversionpt
-										}</li>
-                                        <li>${
-											element.patient.reaction[0]
-												.reactionoutcome
-										}</li>
+                                        <li>${element.patient.reaction[0].reactionmeddrapt}</li>
+                                        <li>${element.patient.reaction[0].reactionmeddraversionpt}</li>
+                                        <li>${element.patient.reaction[0].reactionoutcome}</li>
                                     </ul>
                                     
 			                    </p>
                                 <p class="card-text">
-                                    <h5>Medicinal Product</h5>
-                                    ${product.concat()}
+									<h5>Medicinal Product</h5>
+									<ul>
+										${products}
+									
+									</ul>
 			                    </p>
 			                </div>
 			            </div>
@@ -125,7 +121,92 @@ function recipe() {
                         <div class="card mx-auto" style="width: 18rem;">
                             <img src="${element.image}" class="card-img-top" alt="...">
 			                <div class="card-body">
-			                    <h5 class="card-title">${element.title}</h5>
+			                    <h5 class="card-title" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${element.id}" >${element.title}</h5>
+			                </div>
+			            </div>
+			        </div>
+                `)
+			})
+		},
+	})
+}
+
+$('#exampleModal').on('show.bs.modal', function (e) {
+	let id = e.relatedTarget.dataset.id
+	console.log(id)
+	$.ajax({
+		type: 'GET',
+		url:
+			'https://api.spoonacular.com/recipes/' +
+			id +
+			'/information?apiKey=4d9617e0d6714615a45b3af75282afb5',
+		success: function (response) {
+			console.log(response)
+			// append to modal body
+		},
+	})
+})
+
+function calculate() {
+	$('#main').html('')
+	$.ajax({
+		type: 'GET',
+		crossDomain: true,
+		url:
+			'https://love-calculator.p.rapidapi.com/getPercentage?fname=' +
+			$('#fname').val() +
+			'&sname=' +
+			$('#sname').val(),
+		headers: {
+			'x-rapidapi-key':
+				'7706e67daamshd0dcb0ca98b24a5p1e8f37jsnc502c427dce4',
+			'x-rapidapi-host': 'love-calculator.p.rapidapi.com',
+		},
+		success: function (response) {
+			console.log(response)
+			$()
+			$('#main').append(`
+				<div class="col-12">
+					<div class="card mx-auto" style="width: 18rem;">
+						<div class="card-body">
+							<p class="display-4">${response.percentage}%</p>
+							<h5 class="card-title">${response.result}</h5>
+							<p>first name: ${response.fname}</p>
+							<p>second name: ${response.sname}</p>
+						</div>
+					</div>
+				</div>
+			`)
+		},
+	})
+}
+
+function elaine(news) {
+	$('#main').html('')
+	$.ajax({
+		type: 'GET',
+		crossDomain: true,
+		url:
+			'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/news/' +
+			news +
+			'/0',
+		headers: {
+			'x-rapidapi-key':
+				'7c4795c74dmsh0cf76371648c0b8p12fba6jsn22d42653ab49',
+			'x-rapidapi-host':
+				'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
+			useQueryString: true,
+		},
+		success: function (response) {
+			console.log(response)
+			let result = response.news
+			result.forEach((element) => {
+				$('#main').append(`
+			        <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card mx-auto" style="width: 18rem;">
+			                <div class="card-body">
+			                    <h5 class="card-title"><a href="${element.link}" target="_blank">${element.title}</a></h5>
+			                    <h5 class="card-text">${element.content}</h5>
 			                </div>
 			            </div>
 			        </div>
@@ -141,6 +222,10 @@ $('#btnSearch').on('click', function () {
 
 $('#btnRecipe').on('click', function () {
 	recipe()
+})
+
+$('#btnCalculate').on('click', function () {
+	calculate()
 })
 
 // request data from API
@@ -164,6 +249,12 @@ $('.nav-link').on('click', function (e) {
 			break
 		case 'uk':
 			getNews('ca')
+			break
+		case 'vaccine':
+			elaine('get-vaccine-news')
+			break
+		case 'health':
+			elaine('get-health-news')
 			break
 
 		default:
